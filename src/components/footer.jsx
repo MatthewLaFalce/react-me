3546c2b15c163f80ec85504d1125392b6cbdd620
Matthew LaFalce <mlafalce206@gmail.com>
2022-11-04 12:28:05 -0400

    Add Icon to Footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,6 +4,11 @@ const Footer = () => {
   return (
     <footer className="footer footer-center p-10 bg-primary text-primary-content">
       <div>
+        <div className="avatar">
+          <div className="w-24 mask mask-squircle">
+            <img src="logo200.png" alt="logo" />
+          </div>
+        </div>
         <p className="font-bold">
           Matthew LaFalce <br />
           Providing reliable tech since 2018
