4209bea788636656e438056aa08172b6e07f2cd4
Matthew LaFalce <mlafalce206@gmail.com>
2022-11-03 22:38:47 -0400

    Set default theme to Garden

--- src/components/themePicker.jsx
+++ src/components/themePicker.jsx
@@ -3,11 +3,10 @@ import React from "react";
 const ThemePicker = () => {
   return (
     <select data-choose-theme className="select select-accent">
-      <option value="light">Default</option>
+      <option value="garden">Default</option>
       <option value="corporate">Corporate</option>
       <option value="dracula">Dracula</option>
       <option value="emerald">Emerald</option>
-      <option value="garden">Garden</option>
       <option value="retro">Retro</option>
     </select>
   );
